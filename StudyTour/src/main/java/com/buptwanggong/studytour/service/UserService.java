64b838cd66e1339e77291977a896b02b444b02aa
yangyajie666 <119142998+yangyajie666@users.noreply.github.com>
2024-03-26 22:37:43 +0800

    Delete StudyTour directory

--- StudyTour/src/main/java/com/buptwanggong/studytour/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.buptwanggong.studytour.service;
-
-import com.buptwanggong.studytour.DO.DAO.User;
-import com.buptwanggong.studytour.DO.DTO.UserDTO;
-
-
-public interface UserService {
-    public UserDTO login(String username , String password);
-}
