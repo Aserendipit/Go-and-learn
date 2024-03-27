a79c3226d5ccdb8ee949526f41438cad59b26d65
yangyajie666 <630794886@qq.com>
2024-03-27 19:34:31 +0800

    test push

--- StudyTour/src/main/java/com/buptwanggong/studytour/DO/DTO/UserDTO.java
+++ StudyTour/src/main/java/com/buptwanggong/studytour/DO/DTO/UserDTO.java
@@ -2,7 +2,9 @@ package com.buptwanggong.studytour.DO.DTO;
 
 import com.buptwanggong.studytour.DO.DAO.User;
 import lombok.Data;
-
+/**
+ * 用户DTO类
+ */
 @Data
 public class UserDTO extends User {
     private String token;
