2934e46b504060a12cc8ce452eeb232d78ead186
yangyajie666 <630794886@qq.com>
2024-03-29 19:59:46 +0800

    push test

--- StudyTour/src/main/java/com/buptwanggong/studytour/StudyTourApplication.java
+++ StudyTour/src/main/java/com/buptwanggong/studytour/StudyTourApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class StudyTourApplication {
+public class StudyTourApplication {//
 
 	public static void main(String[] args) {
 		SpringApplication.run(StudyTourApplication.class, args);
